65c342719c9f88927e3fcbe1cc1911ac1a197266
ViniciusFn <contact.viniciusfn@gmail.com>
2025-04-11 17:04:51 -0300

    ✨ Add router and reorganize project structure

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Link } from 'expo-router';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Link href="/login">
+        <Text>Login</Text>
+      </Link>
     </View>
   );
 }
